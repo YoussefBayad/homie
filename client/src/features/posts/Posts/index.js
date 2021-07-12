4a7bee482feaa5a3889d1512f74fa2ecf90c2214
Joseph bayad <jobayad66@gmail.com>
2021-07-12 19:06:16 +0100

    fix profile bug

--- client/src/features/posts/Posts/index.js
+++ client/src/features/posts/Posts/index.js
@@ -9,12 +9,12 @@ import './style.scss';
 const Posts = ({ userId }) => {
   let { data: posts, loading, error } = useSelector((state) => state.posts);
 
-  if (userId) posts = posts.filter((post) => post.user._id === userId);
+  if (userId) posts = posts.filter((post) => post.userId === userId);
   return (
     <div className='posts'>
       {loading && <Spinner style={{ height: '15rem', width: '15rem' }} />}
       {error && <h1>{error.message}</h1>}
-      {posts && posts.map((post) => <Post key={post.id} {...post} />)}
+      {posts && posts.map((post) => <Post key={post._id} {...post} />)}
     </div>
   );
 };
